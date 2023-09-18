bc72c11d5a97d662179ae67655a3db9506888613
Nar0118 <khachatryannarek42@gmail.com>
2023-09-18 10:19:14 +0400

    Added prototypes

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 console.log("Hello world!");
+
+class Test {
+  constructor(name) {
+    this.name = name;
+  }
+
+  hello() {
+    return "hello " + this.name;
+  }
+}
+let obj = new Test("John");
+console.log(obj.__proto__ === Test.prototype); // true
+console.log(obj.__proto__.__proto__ === Object.prototype); // true
+console.log(obj.__proto__.__proto__.__proto__ === null); // true
